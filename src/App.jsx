a6e65b56fcbd5b73bd3af01aebfc2131bcb77928
LeoBasso <127260629+LeoBasso@users.noreply.github.com>
2023-10-17 19:20:58 -0300

    ajustes finais

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-// import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ListLivros from "./components/ListLivros";
 import Detalhes from "./components/Detalhes";
 import EditarLivro from "./components/EditarLivro";
-import AdicionarLivro from "./components/AdicionarLivro"; // Importe o componente AdicionarLivro
+import AdicionarLivro from "./components/AdicionarLivro";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<ListLivros />} /> {/* Página inicial (ListLivros) */}
-        <Route path="/detalhes/:id" element={<Detalhes />} /> {/* Rota para a página de detalhes */}
-        <Route path="/editarLivro/:id" element={<EditarLivro />} /> {/* Rota para a página de edição */}
-        <Route path="/adicionarLivro" element={<AdicionarLivro />} /> {/* Rota para a página de adição */}
+        <Route path="/" element={<ListLivros />} />
+        <Route path="/detalhes/:id" element={<Detalhes />} />
+        <Route path="/editarLivro/:id" element={<EditarLivro />} />
+        <Route path="/adicionarLivro" element={<AdicionarLivro />} />
       </Routes>
     </>
   );
